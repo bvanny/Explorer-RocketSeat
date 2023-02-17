26b74531ea39b9f5bfa16752475e7ee103c289cf
bvanny <bruno_geovanny@outlook.com>
2023-02-17 10:37:45 -0300

    Novo Desafio - 17/02/2023

---
+++ Desafio - GitFav/js/GithubUser.js
@@ -0,0 +1,14 @@
+export class GithubUser {
+  static search(username) {
+    const endPoint = `https://api.github.com/users/${username}`
+
+    return fetch(endPoint)
+      .then(data => data.json())
+      .then(data => ({
+        login: data.login,
+        name: data.name,
+        public_repos: data.public_repos,
+        followers: data.followers,
+      }))
+  }
+}
